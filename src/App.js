ec69ef4fb5375a1752aade3dea6f6809c4ac0a31
Rucha Gavaskar <ruchagavaskar@Ruchas-MacBook-Air.local>
2022-05-11 20:01:22 +0530

    Final Work

--- src/App.js
+++ src/App.js
@@ -1,51 +1,71 @@
-// import FetchWeather from "./Components/FetchWeather";
 import TitleBar from "./Components/TitleBar";
 import WeatherCard from "./Components/WeatherCard";
-import React, { useCallback, useEffect, useState } from "react";
-
-
-
-
+import React, { useCallback,useEffect, useState } from "react";
+import Wave from 'react-wavify'
 function App() {
-  const [weather, setWeather] = useState([{}])
-  // const FetchWeather= async ()=>{
-  //   try {
-  //     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=d08476af38254d32a77141527220905&q=London&aqi=no");
-  //     console.log(response);
-  //     response.json().then((response)=>{
-  //       setWeather(response);
-  //     })
-  //     console.log(weather);
-  //   } catch (error) {
-  //     console.log('my error is'+error)
-  //   }
-  // }
+  
+  const [tempsearch,setTempSearch]= useState("")
+  const [search,setSearch]= useState("");
 
-  // useEffect(()=>{
-  //   FetchWeather();
-  // },[]);
-  const FetchWeather = useCallback( async () => {
-    try {
-      const response = await fetch(
-        'http://api.weatherapi.com/v1/current.json?key=d08476af38254d32a77141527220905&q=London&aqi=no'
-      );
-      console.log(response)
-      setWeather(await response.json());
-      
-    } catch (error) {
-      console.log(error);
-    }
-  },[])
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    setSearch(tempsearch);
+  }
+  const [weather, setWeather] = useState(null);
+  const FetchWeather = useCallback(async () => {
+
+    if(search!=="") { try {
+        const response = await fetch(
+          `http://api.weatherapi.com/v1/forecast.json?key=d08476af38254d32a77141527220905&q=${search}&aqi=no&days=5`
+        );
+        setWeather(await response.json());
+      } catch (error) {
+        console.log(error);
+      }}
+    
+  
+    });
   useEffect(() => {
-  FetchWeather()
-},[FetchWeather]);
+    FetchWeather();
+  }, [search])
   return (
     <div
-      className="bg-white min-h-screen
+      className="bg-white min-h-screen 
     "
     >
       <TitleBar />
-       <WeatherCard weather={weather} />
+      <div  className=" justify-center flex items-center" >
+      <form 
+        className=" mt-20 pl-7 pr-7 rounded-3xl box-borded w-72 h-9 justify-center flex items-center font-poppins bg-[#E3F4FE]
+        "
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="search"
+          id="header-search"
+          placeholder="Search City"
+          name="city"
+          value={tempsearch}
+          onChange={(e)=>setTempSearch(e.target.value)}
+        
+          className=" rounded-3xl  w-64 bg-[#E3F4FE] text-black placeholder-gray pl-2"
+        />
+        &nbsp;&nbsp;
+        <button type="submit">Search</button>
+      </form>
+      </div>
+      {weather === null  ? "" : <WeatherCard weather={weather} />}
+      <Wave fill='#1C9CF6 ' mask="url(#mask)">
+        <defs>
+        <linearGradient id="gradient" gradientTransform="rotate(90)">
+          <stop offset="0" stopColor="white" />
+          <stop offset="0.5" stopColor="black" />
+        </linearGradient>
+        <mask id="mask">
+          <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)"  />
+        </mask>
+      </defs>
+  </Wave>
     </div>
   );
 }
--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -2,59 +2,72 @@ import React from "react";
 // import { WiDaySnow } from "react-icons/wi";
 
 const WeatherCard = ({ weather }) => {
-    // console.log(weather);
+  // console.log(weather);
   return (
+
     <div>
-      <div
-        className="justify-center flex items-center
-        "
-      >
-        <form
-          action="/"
-          method="get"
-          className=" rounded-3xl mt-14  bg-[#E3F4FE] font-poppins  box-border h-10 w-72 text-center"
-        >
-          <input
-            type="text"
-            id="header-search"
-            placeholder="Search City"
-            name="s"
-            className="mt-2 rounded-3xl box-border w-5/12 bg-[#E3F4FE] text-black placeholder-gray"
-          />
-          &nbsp;&nbsp;
-          <button type="submit">Search</button>
-        </form>
-      </div>
       <div className="justify-center flex items-center">
-        <div className="mt-20 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-          <div className="grid grid-cols-4 gap-4">
+        <div className="mt-20 p-6 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="grid grid-cols-6 gap-4">
             <div>
               <h5 className="mb-2  font-poppins text-2xl font-bold tracking-tight text-white ">
                 {weather.location.name}
               </h5>
               <img src={weather.current.condition.icon} alt="" />
             </div>
-            <div>
-              <p className="flex text-white font-poppins text-2xl pl-44 box-border w-60">
-                { weather.current.temp_c} ˚C
+            <div className="flex-col  ">
+              <div className="flex flex-col">
+              <p className="flex text-white font-poppins text-2xl pl-36 box-border w-60">
+                {weather.current.temp_c} ˚C
               </p>
-              <p className="flex text-gray-100 font-poppins text-xl pl-44 box-border w-60">
+              <p className="flex text-gray-100 font-poppins text-xl pl-36 box-border w-60">
                 {weather.current.condition.text}
               </p>
-              {MinMax(24, 19)}
+              </div>
+              
+              <p className="flex pt-1 text-gray-100 font-poppins text-sm pl-32 box-border w-60">
+                <span className="px-4">
+                  {weather.forecast.forecastday[0].day.maxtemp_c}&deg;
+                </span>
+                <span className="px-4">
+                  {weather.forecast.forecastday[0].day.mintemp_c}&deg;
+                </span>
+              </p>
             </div>
           </div>
+          <h1 className="font-poppins text-2xl mt-6 text-white">Forecast</h1>
+         <div className="flex flex-wrap md:flex md:flex-nowrap">
+          
+         
+         {weather.forecast.forecastday.map((curelem) => {
+         const{date,day}=curelem;
+         console.log(curelem)
+         
+         return (
+        
+           <div className="mt-5"> 
+              <p className="flex text-white font-poppins text-xl box-border w-60">
+                {date}
+              </p>
+              <div className="box-border h-10 w-10">
+              <img src={day.condition.icon} alt="" />
+              </div>
+              <p className="flex mt-2 text-gray-100 font-poppins text-sm  box-border w-60">
+                {day.maxtemp_c} ˚C
+              </p>
+              <p className="flex mt-1 text-gray-100 font-poppins text-sm box-border w-60">
+                {day.mintemp_c} ˚C
+              </p>
+            </div>
+    
+         
+         )
+        }
+         )}
+         </div>
         </div>
       </div>
     </div>
   );
 };
-function MinMax(min, max) {
-  return (
-    <p3 className="flex pt-1 text-gray-100 font-poppins text-sm pl-40 box-border w-60">
-      <span className="px-4">{min}&deg;</span>
-      <span className="px-4">{max}&deg;</span>
-    </p3>
-  );
-}
 export default WeatherCard;
